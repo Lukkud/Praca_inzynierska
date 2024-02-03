473ee15b63095feb02f34edc3dacea1f2b37dfc3
Lukkud <lukaszchuchr@gmail.com>
2024-02-03 22:33:39 +0100

    Adding hybrid method for I(w1, w2) calculation

---
+++ Penrose/d2_hybrid_w.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import time
+from d2_utils import D2utils
+
+
+class D2hybridw(D2utils):
+    def __init__(self):
+        try:
+            self.config = self.read_json()['d2_hybrid_w']
+            self.tau = (1 + np.sqrt(5)) * 0.5
+            self.border_1 = self.config["border_1"]
+            self.border_2 = self.config["border_2"]
+            self.source_file = self.config["source_file"]
+            self.threshold = self.config["threshold"]
+            self.df_points = self.read_diffraction_data(self.source_file).astype({'fou': complex})
+        except KeyError as exc:
+            raise Exception(f"No parameter {exc} specified for d2_numerical_k script in config.json file")
+
+    def execute(self):
+        self.df_points["w1"] = 4 * np.pi * self.tau * np.cos(2 * np.pi / 5) * (self.df_points["n1"] + self.df_points["n2"] + (self.df_points["m1"] + self.df_points["m2"]) / self.tau)
+        self.df_points["w2"] = 4 * np.pi * self.tau * np.sin(2 * np.pi / 5) * (self.df_points["n1"] - self.df_points["n2"] + (self.df_points["m1"] - self.df_points["m2"]) / self.tau)
+        self.df_points["phase"] = self.df_points.apply(lambda row: np.arctan(row["fou"].imag / row["fou"].real), axis=1)
+        self.df_points["group"] = self.df_points.apply(lambda row: 3 if (row["phase"] > -self.border_1) and (row["phase"] < self.border_1)
+                                                                     else (1 if (row["phase"] < -self.border_2) or (row["phase"] > self.border_2) else 2), axis=1)
+        self.plotting_w_phase(self.df_points, 'Penrose_plot_w_phase.png', self.threshold, self.border_1, self.border_2)
+        self.plotting_w(self.df_points, 'Penrose_plot_w_hybrid', self.threshold)
+
+if __name__ == "__main__":
+    tic = time.time()
+    x = D2hybridw()
+    x.execute()
+    toc = time.time()
+    print('Overall time: ', round(toc - tic, 2))
+
+
--- Penrose/d2_utils.py
+++ Penrose/d2_utils.py
@@ -22,11 +22,6 @@ class D2utils:
         return config
 
     @staticmethod
-    def saving_data_excel(df, file_name):
-        df = df.round(6)
-        df.to_excel(os.path.join(DATA_PATH, file_name), index=False)
-
-    @staticmethod
     def saving_data_csv(df, file_name):
         df = df.round(6)
         df.to_csv(os.path.join(DATA_PATH, file_name), index=False)
@@ -35,6 +30,11 @@ class D2utils:
     def read_source_data(file_name):
         df = pd.read_csv(os.path.join(SOURCE_PATH, file_name),
                          sep='\t', header=None, names=["x", "y"])
+        return df
+
+    @staticmethod
+    def read_diffraction_data(file_name):
+        df = pd.read_csv(os.path.join(DATA_PATH, file_name))
         return df
 
     @staticmethod
@@ -47,6 +47,71 @@ class D2utils:
         ax.set_xlabel(r"$k_{x}$")
         ax.set_ylabel(r"$k_{y}$")
         ax.set_zlabel(r"$I(k_{x}, k_{y})$")
+        plt.grid(True)
+        ax.view_init(0, 0)
+        plt.savefig(os.path.join(PLOT_PATH, f"{file_name}_0deg.png"), format='png')
+        ax.view_init(90, 0)
+        plt.savefig(os.path.join(PLOT_PATH, f"{file_name}_90deg.png"), format='png')
+        ax.view_init(45, 0)
+        plt.savefig(os.path.join(PLOT_PATH, f"{file_name}_45deg.png"), format='png')
+        ax.view_init(15, 0)
+        plt.savefig(os.path.join(PLOT_PATH, f"{file_name}_15deg.png"), format='png')
+        plt.show()
+
+    @staticmethod
+    def plotting_w_penrose(df, file_name, threshold):
+        df = df.loc[df["iw_fou"] > threshold]
+        plt.rcParams.update({'font.size': 16})
+        fig = plt.figure(figsize=(15, 10))
+        ax = plt.axes(projection="3d")
+        ax.scatter3D(df.k1, df.k2, df.iw_fou, color="black")
+        ax.set_xlabel(r"$k_{x}$")
+        ax.set_ylabel(r"$k_{y}$")
+        ax.set_zlabel(r"$I(k_{x}, k_{y})$")
+        plt.grid(True)
+        ax.view_init(0, 0)
+        plt.savefig(os.path.join(PLOT_PATH, f"{file_name}_0deg.png"), format='png')
+        ax.view_init(90, 0)
+        plt.savefig(os.path.join(PLOT_PATH, f"{file_name}_90deg.png"), format='png')
+        ax.view_init(45, 0)
+        plt.savefig(os.path.join(PLOT_PATH, f"{file_name}_45deg.png"), format='png')
+        ax.view_init(15, 0)
+        plt.savefig(os.path.join(PLOT_PATH, f"{file_name}_15deg.png"), format='png')
+        plt.show()
+
+    @staticmethod
+    def plotting_w_phase(df, file_name, threshold, border_1, border_2):
+        df = df.loc[df["iw_fou"] > threshold].sort_values('phase').reset_index(drop=True)
+
+        df_group_1 = df.loc[df["group"] == 1]
+        df_group_2 = df.loc[df["group"] == 2]
+        df_group_3 = df.loc[df["group"] == 3]
+
+        plt.rcParams.update({'font.size': 22})
+        fig = plt.figure(figsize=(15, 10))
+        plt.plot(df_group_1.index.values, df_group_1.phase, 'o', color=(0.1, 0.9, 0.1), ms=1, zorder=1)
+        plt.plot(df_group_2.index.values, df_group_2.phase, 'o', color="red", ms=1, zorder=2)
+        plt.plot(df_group_3.index.values, df_group_3.phase, 'o', color="blue", ms=1, zorder=3)
+        plt.axhline(y=border_1, ls='--', linewidth=2, color='blue')
+        plt.axhline(y=border_2, ls='--', linewidth=2, color='red')
+        plt.axhline(y=-border_1, ls='--', linewidth=2, color='blue')
+        plt.axhline(y=-border_2, ls='--', linewidth=2, color='red')
+        plt.xlabel(r"Numer piku")
+        plt.ylabel(r"Wartość fazy $\phi$")
+        plt.grid(True)
+        plt.savefig(os.path.join(PLOT_PATH, file_name), format='png')
+        plt.show()
+
+    @staticmethod
+    def plotting_w(df, file_name, threshold):
+        df = df.loc[df["iw_fou"] > threshold]
+        plt.rcParams.update({'font.size': 16})
+        fig = plt.figure(figsize=(10, 10))
+        ax = plt.axes(projection='3d')
+        ax.scatter3D(df.w1, df.w2, df.iw_fou, s=4, color="black")
+        ax.set_xlabel(r"$w_{x}$")
+        ax.set_ylabel(r"$w_{y}$")
+        ax.set_zlabel(r"$I(w_{x}, w_{y})$")
         plt.grid(True)
         ax.view_init(0, 0)
         plt.savefig(os.path.join(PLOT_PATH, f"{file_name}_0deg.png"), format='png')
